33a7688f1cf1faca4bd67d6256dccad5bf385e12
roberto-fernandino <romfernandino@gmail.com>
2024-09-27 02:05:32 -0300

    feat: Three new endpoints
    Get user
    Add wallet to user
    Get user wallets

--- backend_app/src/main.rs
+++ backend_app/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use crate::routes::{index, add_or_update_user, get_all_users, get_user};
+use crate::routes::{index, add_or_update_user, get_all_users, get_user, add_wallet_to_user, get_user_wallets};
 mod routes;
 mod utils;
 
@@ -11,7 +11,9 @@ async fn main() -> Result<()> {
     app.at("/").get(index);
     app.at("/add_or_update_user").post(add_or_update_user);
     app.at("/all_users").get(get_all_users);
-    app.at("/user/:id").get(get_user);
+    app.at("/user/:user_id").get(get_user);
+    app.at("/add_wallet_to_user").post(add_wallet_to_user);
+    app.at("/user_wallets/:user_id").get(get_user_wallets);
     app.listen("0.0.0.0:8000").await?;
 
     Ok(())
