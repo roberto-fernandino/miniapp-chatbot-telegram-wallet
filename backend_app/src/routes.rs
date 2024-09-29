8c5d96b38a3e11eafd50ac6e611c3877c4ad4a47
roberto-fernandino <romfernandino@gmail.com>
2024-09-29 13:08:39 -0300

    feat: New route to set session for the user.

--- backend_app/src/routes.rs
+++ backend_app/src/routes.rs
@@ -2,7 +2,6 @@ use crate::utils::helpers::get_redis_connection;
 use std::collections::HashMap;
 use redis::Commands;
 use serde::{Serialize, Deserialize};
-use anyhow::Result;
 
 
 pub async fn index(_req: tide::Request<()>) -> tide::Result<String> {
@@ -10,147 +9,62 @@ pub async fn index(_req: tide::Request<()>) -> tide::Result<String> {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    pub id: String,
-    pub first_name: String,
-    pub last_name: String,
-    pub username: String,
-    pub language_code: String,
-    pub allows_write_to_pm: bool,
-}
-
-
-pub async fn check_user_exists(user_id: &str) -> Result<bool> {
-    let mut con = get_redis_connection().await?;
-    let user_exists: bool = con.sismember("users", format!("user:{}", user_id))?;
-    Ok(user_exists)
-}
-
-/// This route is used to add or update a user.
-/// It is used to add a new user or update an existing user.
-pub async fn add_or_update_user(mut req: tide::Request<()>) -> tide::Result<String> {
-    let mut con = get_redis_connection().await?;
-    let user: User = req.body_json().await?;
-
-    // Store user as hash
-    con.hset_multiple(
-        format!("user:{}", user.id),
-        &[
-            ("username", &user.username),
-            ("first_name", &user.first_name),
-            ("last_name", &user.last_name),
-            ("language_code", &user.language_code),
-            ("allows_write_to_pm", &user.allows_write_to_pm.to_string()),
-        ],
-    )?;
-
-    // Add user ID to the set of all users
-    con.sadd("users", format!("user:{}", user.id))?;
-
-    Ok("User added or updated or touched successfully".to_string())
-}
-
-pub async fn get_user(req: tide::Request<()>) -> tide::Result<String> {
-    let user_id = req.param("user_id")?;
-    let mut con = get_redis_connection().await?;
-    let user_hash: HashMap<String, String> = con.hgetall(format!("user:{}", user_id))?;
-    if user_hash.is_empty() {
-        return Ok("User not found".to_string());
-    }
-
-    let user = User {
-        id: user_id.to_string(),
-        first_name: user_hash.get("first_name").unwrap().to_string(),
-        last_name: user_hash.get("last_name").unwrap().to_string(),
-        username: user_hash.get("username").unwrap().to_string(),
-        language_code: user_hash.get("language_code").unwrap().to_string(),
-        allows_write_to_pm: user_hash.get("allows_write_to_pm").unwrap().to_string().parse::<bool>().unwrap_or(false),
-    };
-
-    Ok(serde_json::to_string(&user)?)
-}
-
-
-pub async fn get_all_users(_req: tide::Request<()>) -> tide::Result<String> {
-    println!("get_all_users_id");
-    let mut con = get_redis_connection().await?;
-    
-    // Get all user IDs from the set
-    let user_ids: Vec<String> = con.smembers("users")?;
-    Ok(serde_json::to_string(&user_ids)?)
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct WalletPost {
-    pub user_id: String,
-    pub wallet_id: String,
-    pub turnkey_wallet_name: String,
-    pub user_wallet_name: String,
-    pub sol_address: String,
-}
-
-pub async fn add_wallet_to_user(mut req: tide::Request<()>) -> tide::Result<String> {
-    let post: WalletPost = req.body_json().await?;
-
-    let mut con = get_redis_connection().await?;
-
-    let key = format!("user:{}:wallet_id:{}", post.user_id, post.wallet_id);
-    con.hset_multiple (key, &[
-        ("wallet_id", post.wallet_id),
-        ("sol_address", post.sol_address),
-        ("turnkey_wallet_name", post.turnkey_wallet_name),
-        ("user_wallet_name", post.user_wallet_name),
-    ])?;
-
-    Ok("Wallet added to user".to_string())
-}
-
-
-pub async fn get_user_wallets(req: tide::Request<()>) -> tide::Result<String> {
-    let user_id = req.param("user_id")?;
-    let mut con = get_redis_connection().await?;
-
-
-    // get all wallets for user
-    let wallets_keys: Vec<String> = con.keys(format!("user:{}:wallet_id:*", user_id))?;
-
-    // get all data for each wallet
-    let mut wallets = Vec::new();
-    for key in &wallets_keys {
-        let wallet_data: HashMap<String, String> = con.hgetall(key)?;
-        wallets.push(wallet_data);
-    }
-
-    // return as json
-    Ok(serde_json::to_string(&wallets)?)
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 pub struct CopyTradeWalletPost {
     pub user_id: String,
     pub wallet_id: String,
+    pub account_address: String,
     pub buy_amount: String,
     pub copy_trade_address: String,
     pub status: String,
-    pub user_wallet_name: String,
 }
 
 pub async fn set_copy_trade_wallet(mut req: tide::Request<()>) -> tide::Result<String> {
     let post: CopyTradeWalletPost = req.body_json().await?;
     let mut con = get_redis_connection().await?;
-
+    
     let key = format!("user:{}:copy_trade_wallet:{}", &post.user_id, &post.copy_trade_address);
     con.hset_multiple(key, &[
+        ("user_id", &post.user_id),
         ("wallet_id", &post.wallet_id),
+        ("account_address", &post.account_address),
         ("buy_amount", &post.buy_amount),
         ("status", &post.status),
         ("copy_trade_address", &post.copy_trade_address),
-        ("user_wallet_name", &post.user_wallet_name),
-    ])?;
-
+        ])?;
+        
+    let response = surf::get("http://solana_app:3030/resubscribe").recv_string().await;
+    match response {
+        Ok(_) => {
+            println!("Subscribed");
+        }
+        Err(e) => {
+            eprintln!("Failed to update Solana app: {}", e);
+            return Ok("Failed to update Solana app".to_string());
+        }
+    }
     Ok(serde_json::to_string(&post)?)
 }
 
+pub async fn delete_copy_trade_wallet(req: tide::Request<()>) -> tide::Result<String> {
+    let user_id = req.param("user_id")?;
+    let copy_trade_address = req.param("copy_trade_address")?;
+    let mut con = get_redis_connection().await?;
+
+    let key = format!("user:{}:copy_trade_wallet:{}", user_id, copy_trade_address);
+    con.del(key)?;
+
+    let response = surf::get("http://solana_app:3030/resubscribe").recv_string().await;
+    match response {
+        Ok(_) => {
+            println!("Unsubscribed");
+        }
+        Err(e) => {
+            eprintln!("Failed to update Solana app: {}", e);
+            return Ok("Failed to update Solana app".to_string());
+        }
+    }
+    Ok("Copy trade wallet deleted".to_string())
+}
 
 pub async fn get_copy_trades(req: tide::Request<()>) -> tide::Result<String> {
     let user_id = req.param("user_id")?;
@@ -164,4 +78,39 @@ pub async fn get_copy_trades(req: tide::Request<()>) -> tide::Result<String> {
     }
 
     Ok(serde_json::to_string(&copy_trades)?)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserSessionPost {
+    pub user_id: String,
+    pub session_end_time: String,
+    pub public_key: String,
+    pub private_key: String,
+}
+
+pub async fn set_user_session(mut req: tide::Request<()>) -> tide::Result<String> {
+    let post: UserSessionPost = match req.body_json().await {
+        Ok(post) => post,
+        Err(e) => {
+            eprintln!("Failed to parse request body: {:?}", e);
+            return Err(tide::Error::from_str(422, "Invalid request body"));
+        }
+    };
+    
+    let mut con = match get_redis_connection().await {
+        Ok(con) => con,
+        Err(e) => {
+            eprintln!("Failed to get Redis connection: {:?}", e);
+            return Err(tide::Error::from_str(500, "Internal server error"));
+        }
+    };
+
+    let key = format!("user:{}:session", &post.user_id);
+    con.hset_multiple(key, &[
+        ("user_id", &post.user_id),
+        ("session_end_time", &post.session_end_time),
+        ("public_key", &post.public_key),
+        ("private_key", &post.private_key),
+    ])?;
+    Ok("Session set".to_string())
 }
