2d59eb72795af2a1d4dc02c8043814abfe4864a4
roberto-fernandino <romfernandino@gmail.com>
2024-10-10 17:27:30 -0300

    Added documentation.

--- telegram_bot/src/main.rs
+++ telegram_bot/src/main.rs
@@ -62,16 +62,7 @@ async fn handle_message(bot: Bot, msg: Message) -> Result<(), Box<dyn std::error
         else if there_is_valid_solana_address(text) {
             // Get the valid solana address
             let address = get_valid_solana_address(text);
-            // First call info
-            let mut call_info_str = String::new();
-            let is_first_call = db::is_first_call(&con,address.as_ref().unwrap(), msg.chat.id.to_string().as_str());
-            if !is_first_call {
-                let first_call = db::get_first_call_token_chat(&con, address.as_ref().unwrap(), msg.chat.id.to_string().as_str());
-                if let Some(first_call) = first_call{
-                    let user_called_first = db::get_user(&con, first_call.user_tg_id.as_str()).expect("User not found");
-                    call_info_str.push_str(&format!("😈 <a href=\"https://t.me/sj_copyTradebot?start=user_{}\"><i><b>{}</b></i></a> @ {}", first_call.user_tg_id,  user_called_first.username, format_number(first_call.mkt_cap.parse::<f64>().unwrap_or(0.0))));
-                }
-            } 
+            let call_info_str = utils::helpers::get_call_info(&address.clone().expect("No address found"), &con, &msg);
             match address {
                 Some(address) => {
                     // Call the address
@@ -94,20 +85,22 @@ async fn handle_message(bot: Bot, msg: Message) -> Result<(), Box<dyn std::error
 /// 
 /// * `bot` - The bot structure
 /// * `query` - The callback query structure
+/// * `msg` - The message structure
 /// 
 /// # Returns
 /// 
 /// * `Ok(())` - The operation was successful
 /// * `Err(e)` - The operation failed
 async fn handle_callback_query(bot: Bot, query: CallbackQuery) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    
+     
     if let Some(data) = query.data.as_ref() {
         if data.starts_with("del_call:") {
             match handle_callback_del_call(data.to_string(), &bot, &query).await {
                 Ok(_) => (),
                 Err(e) => log::error!("Failed to delete call: {:?}", e),
             }
-        } else {
+        } 
+        else {
             log::info!("Unrecognized callback query data: {}", data);
         }
     } else {
