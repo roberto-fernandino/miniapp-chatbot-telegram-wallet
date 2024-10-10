c7370a5edfc10b801603b898494796ec28452cbd
roberto-fernandino <romfernandino@gmail.com>
2024-10-10 20:10:30 -0300

    feat: Removed logic to run inexistent script.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-if echo "$DOCKER_TAGS" | grep -q "dev"; then
-  echo "Running in development mode"
-  exec npm run dev -- --host
-else
-  echo "Running in production mode"
-  exec npm start
-fi
+exec npm run dev -- --host
