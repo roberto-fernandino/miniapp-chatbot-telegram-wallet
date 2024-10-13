4ea40e331804ee4d1c51dc538273817cff991ed5
roberto-fernandino <romfernandino@gmail.com>
2024-10-13 10:08:27 -0300

    feat: Added scanner_response to get the % diference after the call on the call info.

--- telegram_bot/src/utils/helpers.rs
+++ telegram_bot/src/utils/helpers.rs
@@ -1,7 +1,11 @@
+use chrono::{TimeDelta, TimeZone};
 use sqlite::Connection;
+use std::sync::{Arc, Mutex};
 use anyhow::Result;
-use crate::format_number;
+use crate::{format_age, format_number};
 use crate::db::*;
+use crate::get_pair_token_pair_and_token_address;
+use crate::get_scanner_search;
 use teloxide::types::Message;
 use chrono::{NaiveDateTime, Utc, DateTime};
 
@@ -139,16 +143,36 @@ pub async fn time_to_timestamp(time: &str) -> i64 {
 /// # Returns
 /// 
 /// A string containing the call info
-pub fn get_call_info(address: &String, con: &Connection, msg: &Message) -> String {
+pub async fn get_call_info(address: &String, con: &Arc<Mutex<Connection>>, msg: &Message) -> Result<String> {
     // First call info
     let mut call_info_str = String::new();
     let is_first_call = is_first_call(&con,address.as_ref(), msg.chat.id.to_string().as_str());
+    let token_pair_and_token_address  = get_pair_token_pair_and_token_address(address).await?;
+    let scanner_response = get_scanner_search(token_pair_and_token_address["pairAddress"].as_str().unwrap_or(""), token_pair_and_token_address["tokenAddress"].as_str().unwrap_or("")).await?;
+    let mkt_cap = scanner_response["pair"]["token1TotalSupplyFormatted"].as_str().unwrap_or("0").parse::<f64>().unwrap_or(0.0) * scanner_response["pair"]["pairPrice1Usd"].as_str().unwrap_or("0").parse::<f64>().unwrap_or(0.0);
     if !is_first_call {
-        let first_call = get_first_call_token_chat(&con, address.as_ref(), msg.chat.id.to_string().as_str());
+        let first_call = {
+            let con = con.lock().unwrap();
+            get_first_call_token_chat(&con, address.as_ref(), msg.chat.id.to_string().as_str())
+        };
+
+        // Calculating the % change
+        let first_call_mkt_cap = first_call.clone().expect("First call not found").mkt_cap.parse::<f64>().unwrap_or(0.0);
+        let percentage_change = format_number(((mkt_cap - first_call_mkt_cap) / first_call_mkt_cap) * 100.0);
+
+
+        // Calculating the age of the call
+        let timestamp = time_to_timestamp(&first_call.clone().expect("First call doesn't exist.").time).await;
+        let call_time = Utc.timestamp_millis_opt(timestamp).unwrap();
+        let current_time = Utc::now();
+        let time_delta: TimeDelta = current_time.signed_duration_since(call_time);
         if let Some(first_call) = first_call{
-            let user_called_first = get_user(&con, first_call.user_tg_id.as_str()).expect("User not found");
-            call_info_str.push_str(&format!("😈 <a href=\"https://t.me/sj_copyTradebot?start=user_{}\"><i><b>{}</b></i></a> @ {}", first_call.user_tg_id,  user_called_first.username, format_number(first_call.mkt_cap.parse::<f64>().unwrap_or(0.0))));
+            let user_called_first = {
+                let con = con.lock().unwrap();
+                get_user(&con, first_call.user_tg_id.as_str()).expect("User not found")
+            };
+            call_info_str.push_str(&format!("😈 <a href=\"https://t.me/sj_copyTradebot?start=user_{}\"><i><b>{}</b></i></a> @ {} <b>[{}%]</b> ({})", first_call.user_tg_id,  user_called_first.username, format_number(first_call.mkt_cap.parse::<f64>().unwrap_or(0.0)), percentage_change, format_age(time_delta)));
         }
     } 
-    call_info_str
+    Ok(call_info_str)
 }
