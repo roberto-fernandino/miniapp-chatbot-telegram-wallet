a3c4efc5649540389790e01b20f78292d1dc5868
roberto-fernandino <romfernandino@gmail.com>
2024-10-21 10:13:19 -0300

    feat: Created swap signed with turnkey.

--- solana_app/src/modules/swap.rs
+++ solana_app/src/modules/swap.rs
@@ -1,12 +1,10 @@
 use {
     super::matis::SwapTransaction, crate::turnkey::{
-        client::{KeySelector, Turnkey}, errors::TurnkeyResult
-    }, solana_client::rpc_client::RpcClient, solana_sdk::{
+        client::{KeyInfo, KeySelector, Turnkey}, errors::TurnkeyResult
+    }, bincode::deserialize, solana_client::rpc_client::RpcClient, solana_sdk::{
         commitment_config::CommitmentConfig, message::Message, pubkey::Pubkey,
-        system_instruction, transaction::Transaction,
-    }, std::{env, str::FromStr},
-    solana_sdk::transaction::VersionedTransaction,
-    bincode::deserialize,
+        system_instruction, transaction::{Transaction, VersionedTransaction},
+    }, std::{env, str::FromStr}
 };
 
 struct User {
@@ -17,14 +15,32 @@ struct User {
 }
 
 async fn sign_swap_transaction(transaction: SwapTransaction, user: User) -> TurnkeyResult<()> {
+    // Initialize Turnkey client
     let turnkey_client = Turnkey::new_for_user(&user.api_public_key, &user.api_private_key, &user.organization_id, &user.public_key)?;
-    let rpc_client = RpcClient::new(env::var("NODE_HTTP").expect("NODE_HTTP must be set"));
     let pubkey = Pubkey::from_str(&user.public_key).expect("Invalid pubkey");
-    let transaction_data = base64::decode(transaction.swap_transaction).expect("Invalid transaction data");
-    let transaction = deserialize::<VersionedTransaction>(&transaction_data[..]).expect("Invalid transaction");
-    let recent_blockhash = rpc_client.get_latest_blockhash().expect("Failed to get latest blockhash");
 
-    // let (tx, _sig) = turnkey_client.sign_transaction(transaction, KeySelector::ExampleKey, recent_blockhash);
+    // Initialize RPC client
+    let rpc_client = RpcClient::new(env::var("NODE_HTTP").expect("NODE_HTTP must be set"));
+
+    // Decode transaction
+    let transaction_data = base64::decode(transaction.swap_transaction).expect("Invalid transaction data");
+    let mut transaction = deserialize::<Transaction>(&transaction_data[..]).expect("Invalid transaction");
+
+    // Get latest blockhash
+    let key_info = KeyInfo {
+       private_key_id: user.public_key,
+       public_key: pubkey
+    };
+
+    // Sign transaction
+    println!("Signing transaction");
+    let (tx, _sig) = turnkey_client.sign_transaction(&mut transaction, key_info).await?;
+    println!("Transaction signed");
+
+    println!("Sending transaction");
+    let tx_sig = rpc_client.send_and_confirm_transaction(&tx).expect("Failed to send transaction");
+    println!("Transaction confirmed: {:?}", tx_sig);
+
     Ok(())
 
 }
