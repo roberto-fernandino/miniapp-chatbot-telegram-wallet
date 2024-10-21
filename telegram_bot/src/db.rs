7cea112b4c1505c407230643c242ac1bf7498dc1
roberto-fernandino <romfernandino@gmail.com>
2024-10-21 01:12:41 -0300

    fix: Fix borrowing error.

--- telegram_bot/src/db.rs
+++ telegram_bot/src/db.rs
@@ -954,15 +954,17 @@ pub async fn add_user(pool: &PgPool, post_user_request: PostUserRequest) -> Resu
 /// # Returns
 /// 
 /// A boolean indicating whether the user is registered in the mini app
-pub async fn is_user_registered_in_mini_app(pool: &PgPool,msg: &teloxide::types::Message) -> Result<bool> {
-    let user_tg_id = msg.from.clone().unwrap().id.to_string().as_str();
-    let username = msg.from.clone().unwrap().username.unwrap_or("Unknown".to_string()).as_str();
-    let user_exists = user_exists(pool, user_tg_id).await?;
+pub async fn is_user_registered_in_mini_app(pool: &PgPool, msg: &teloxide::types::Message) -> Result<bool> {
+    let from = msg.from.clone().unwrap();
+    let user_tg_id = from.id.to_string();
+    let username = from.username.unwrap_or_else(|| "Unknown".to_string());
+    
+    let user_exists = user_exists(pool, &user_tg_id).await?;
     if user_exists {
-        let user = get_user_by_tg_id(pool, user_tg_id).await?;
+        let user = get_user_by_tg_id(pool, &user_tg_id).await?;
         Ok(user.solana_address != "" && user.eth_address != "" && user.turnkey_info.api_public_key != "" && user.turnkey_info.api_private_key != "")
     } else {
-        create_user_with_tg_id_and_username(pool, user_tg_id, Some(username));
+        create_user_with_tg_id_and_username(pool, &user_tg_id, Some(&username)).await?;
         Ok(false)
     }
 }   
