2d35280a4abf036c477ab4cfd6eee13ca47d8741
roberto-fernandino <romfernandino@gmail.com>
2024-10-21 20:22:54 -0300

    feat: Logging.

--- solana_app/src/modules/swap.rs
+++ solana_app/src/modules/swap.rs
@@ -17,30 +17,37 @@ pub struct User {
 
 pub async fn sign_and_send_swap_transaction(transaction: SwapTransaction, user: User) -> TurnkeyResult<Signature> {
     // Initialize Turnkey client
+    println!("@sign_and_send_swap_transaction/ user: {:?}", user);
+    println!("@sign_and_send_swap_transaction/ transaction: {:?}", transaction);
     let turnkey_client = Turnkey::new_for_user(&user.api_public_key, &user.api_private_key, &user.organization_id, &user.public_key)?;
+    println!("@sign_and_send_swap_transaction/ turnkey_client created");
     let pubkey = Pubkey::from_str(&user.public_key).expect("Invalid pubkey");
 
     // Initialize RPC client
     let rpc_client = RpcClient::new(env::var("NODE_HTTP").expect("NODE_HTTP must be set"));
+    println!("@sign_and_send_swap_transaction/ connected to RPC client");
 
     // Decode transaction
+    println!("@sign_and_send_swap_transaction/ decoding transaction");
     let transaction_data = base64::decode(transaction.swap_transaction).expect("Invalid transaction data");
+    println!("@sign_and_send_swap_transaction/ transaction decoded");
     let mut transaction = deserialize::<Transaction>(&transaction_data[..]).expect("Invalid transaction");
+    println!("@sign_and_send_swap_transaction/ transaction deserialized");
 
     // Get latest blockhash
     let key_info = KeyInfo {
        private_key_id: user.public_key,
        public_key: pubkey
     };
-
+    println!("@sign_and_send_swap_transaction/ key_info created");
     // Sign transaction
-    println!("Signing transaction");
+    println!("@sign_and_send_swap_transaction/ signing transaction");
     let (tx, _sig) = turnkey_client.sign_transaction(&mut transaction, key_info).await?;
-    println!("Transaction signed");
+    println!("@sign_and_send_swap_transaction/ transaction signed");
 
-    println!("Sending transaction");
+    println!("@sign_and_send_swap_transaction/ sending transaction");
     let tx_sig = rpc_client.send_and_confirm_transaction(&tx).expect("Failed to send transaction");
-    println!("Transaction confirmed: {:?}", tx_sig);
+    println!("@sign_and_send_swap_transaction/ transaction confirmed: {:?}", tx_sig);
 
     Ok(tx_sig)
 
