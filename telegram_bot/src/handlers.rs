073359855142cbb20e1e279708b19ad286c43f89
roberto-fernandino <romfernandino@gmail.com>
2024-10-23 13:40:49 -0300

    Removed back handle that doesnt work well yet.

--- telegram_bot/src/handlers.rs
+++ telegram_bot/src/handlers.rs
@@ -332,14 +332,6 @@ pub async fn handle_callback_query(
                 Err(e) => log::error!("Failed to set custom buy amount: {:?}", e),
             }
         }
-        else if data == "back" {
-            if let Some(teloxide::types::MaybeInaccessibleMessage::Regular(msg)) = query.message.as_ref() {
-                if let Some(from) = msg.from.as_ref() {
-                    println!("@handle_callback_query/ from: {:?}", from);
-                    start(&bot, msg, &pool).await?;
-                }
-            }
-        }
         else {
             log::info!("Unrecognized callback query data: {}", data);
         }
