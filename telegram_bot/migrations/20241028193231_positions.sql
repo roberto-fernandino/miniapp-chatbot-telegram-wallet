4a39db4b8afc99f41eb6f41fce9c1605030e65b4
roberto-fernandino <romfernandino@gmail.com>
2024-10-28 16:54:25 -0300

    feat: New table positions to save users open positions.

---
+++ telegram_bot/migrations/20241028193231_positions.sql
@@ -0,0 +1,13 @@
+-- Add migration script here
+
+-- Creating positions table
+CREATE TABLE IF NOT EXISTS positions (
+    id SERIAL PRIMARY KEY,
+    tg_user_id INTEGER NOT NULL,
+    token_address TEXT NOT NULL,
+    take_profits JSONB NOT NULL,
+    stop_losses JSONB NOT NULL,
+    amount FLOAT NOT NULL,
+    mc_entry FLOAT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
