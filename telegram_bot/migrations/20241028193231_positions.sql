35a8b7374fad45318276ee1ab2c33fcd4cb429b0
roberto-fernandino <romfernandino@gmail.com>
2024-10-30 11:45:23 -0300

    feat: TP/SL Not null with empty array as default for position.

--- telegram_bot/migrations/20241028193231_positions.sql
+++ telegram_bot/migrations/20241028193231_positions.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS positions (
     id SERIAL PRIMARY KEY,
     tg_user_id INTEGER NOT NULL,
     token_address TEXT NOT NULL,
-    take_profits JSONB NOT NULL,
-    stop_losses JSONB NOT NULL,
+    take_profits JSONB NOT NULL DEFAULT '[]',
+    stop_losses JSONB NOT NULL DEFAULT '[]',
     amount FLOAT NOT NULL,
     mc_entry FLOAT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
