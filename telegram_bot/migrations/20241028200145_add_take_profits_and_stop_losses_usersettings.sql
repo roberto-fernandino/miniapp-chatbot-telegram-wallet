340df8e52ef9a960b04fa892df849c7eebd7df4f
roberto-fernandino <romfernandino@gmail.com>
2024-10-30 11:43:11 -0300

    feat: Changed migration to create TP/SL not null with a default array as value.

--- telegram_bot/migrations/20241028200145_add_take_profits_and_stop_losses_usersettings.sql
+++ telegram_bot/migrations/20241028200145_add_take_profits_and_stop_losses_usersettings.sql
@@ -1,5 +1,5 @@
 -- Add migration script here
 
 -- Add take_profits and stop_losses to user_settings
-ALTER TABLE user_settings ADD COLUMN take_profits JSONB;
-ALTER TABLE user_settings ADD COLUMN stop_losses JSONB;
+ALTER TABLE user_settings ADD COLUMN take_profits JSONB NOT NULL DEFAULT '[]';
+ALTER TABLE user_settings ADD COLUMN stop_losses JSONB NOT NULL DEFAULT '[]';
