b421dd50afaf1b163a6ef15636ac2726362d4664
roberto-fernandino <romfernandino@gmail.com>
2024-11-01 16:03:14 -0300

    feat: Get wallet aamount get ui amount too now.

--- telegram_bot/src/utils/helpers.rs
+++ telegram_bot/src/utils/helpers.rs
@@ -1407,13 +1407,23 @@ pub async fn check_tokens_prices(token_addresses: Vec<String>) -> Result<HashMap
 /// 
 /// # Returns
 /// 
-/// A f64 representing the token amount
-pub async fn get_token_amount_in_wallet(solana_wallet_address: &str, token_address: &str) -> Result<f64> {
+/// A tuple representing the token amount and the UI amount
+pub async fn get_token_amount_in_wallet(solana_wallet_address: &str, token_address: &str) -> Result<(f64, String)> {
     let positions = get_positions_balance(solana_wallet_address).await?;
-    Ok(positions["tokens"].as_array().unwrap_or(&Vec::new())
+    let empty_vec = Vec::new(); // Create a binding for the empty vector
+    let tokens_array = positions["tokens"].as_array().unwrap_or(&empty_vec);
+    let token_amount = tokens_array
         .iter()
         .find(|token| token["mint"].as_str().unwrap_or("") == token_address)
         .unwrap_or(&serde_json::Value::Null)["token_amount"]  
         .as_f64()
-        .unwrap_or(0.0))
+        .unwrap_or(0.0);
+    let ui_amount = tokens_array
+        .iter()
+        .find(|token| token["mint"].as_str().unwrap_or("") == token_address)
+        .unwrap_or(&serde_json::Value::Null)["token_ui_amount"]
+        .as_str()
+        .unwrap_or("0");
+    Ok((token_amount, ui_amount.to_string()))
 }
+
