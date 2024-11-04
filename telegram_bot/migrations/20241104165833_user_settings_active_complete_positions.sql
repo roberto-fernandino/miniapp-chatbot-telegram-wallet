a0ab1e1bfa19455c15b29139688f4789060e7201
roberto-fernandino <romfernandino@gmail.com>
2024-11-04 16:42:35 -0300

    feat:  Made active complete positons not null with default beeing active.

--- telegram_bot/migrations/20241104165833_user_settings_active_complete_positions.sql
+++ telegram_bot/migrations/20241104165833_user_settings_active_complete_positions.sql
@@ -2,4 +2,4 @@
 
 
 -- Add the active_complete_positions column to the user_settings table
-ALTER TABLE user_settings ADD COLUMN active_complete_positions VARCHAR(255); 
+ALTER TABLE user_settings ADD COLUMN active_complete_positions VARCHAR(255) NOT NULL DEFAULT 'active'; 
