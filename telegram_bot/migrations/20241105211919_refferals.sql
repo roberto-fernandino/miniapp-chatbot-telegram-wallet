d9b456a9d389739ebb03089f91034a7c99106e45
roberto-fernandino <romfernandino@gmail.com>
2024-11-05 18:26:14 -0300

    feat: Referrals.

---
+++ telegram_bot/migrations/20241105211919_refferals.sql
@@ -0,0 +1,16 @@
+-- Add migration script here
+
+
+-- Add a table for refferals
+CREATE TABLE IF NOT EXISTS refferals (
+    id SERIAL PRIMARY KEY,
+    user_tg_id VARCHAR(255) NOT NULL UNIQUE,
+    uuid VARCHAR(255) NOT NULL UNIQUE,
+    users_referred INTEGER DEFAULT 0,
+    referral_rebates INTEGER DEFAULT 0,
+    total_rewards NUMERIC DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE users ADD COLUMN refferal_id INTEGER REFERENCES refferals(id);
